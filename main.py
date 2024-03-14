f508344fab7c2d1210b171f420d1e75f0c76addf
jozeto4584 <jozetoz4584@gmail.com>
2024-03-13 19:56:11 -0500

    nt5 feat se configura codigo

---
+++ main.py
@@ -0,0 +1,28 @@
+userNameBD='hidro45'
+userPaswordBD='admin123'
+
+userName=input('Digita tu Usuario:')
+userPasword=input('Difita su Contraseña:')
+
+'''if(userName!=userNameBD){
+    
+}'''
+
+if userName!=userNameBD and userPasword==userPaswordBD:
+    print("exito")
+    
+else:
+    print("credenciales erroneas")    
+    
+    
+print("oe")
+
+# si esta tabulado esta deltro del bloque
+
+# let me permite crear variables globales 
+# var permite crar variables globales y locales 
+
+#Comparando las variables 
+
+
+
